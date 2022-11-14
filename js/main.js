78847bcedb568cb75808d673ffc812af5f954acf
Michele Nardini <michele.nardini3g@gmail.com>
2022-11-14 22:49:30 +0100

    update

--- js/main.js
+++
@@ -1,3 +0,0 @@
-import * as THREE from 'js/three.js';
-
-const scene = new THREE.Scene();
---
+++ main.js
@@ -0,0 +1,27 @@
+import * as THREE from 'https://unpkg.com/three@0.146.0/build/three.module.js';
+
+
+document.addEventListener("DOMContentLoaded", initializeScene());
+
+function initializeScene(){
+    const scene = new THREE.Scene();
+    
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({color: "#0000FF"});
+
+    const cube = new THREE.Mesh(geometry, material);
+
+    scene.add(cube);
+    cube.position.set(0, 0, -2);
+    cube.rotation.set(0, Math.PI/4, 0);
+
+    const camera = new THREE.PerspectiveCamera();
+    camera.position.set(1, 1, 5);
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(500,500);
+
+    renderer.render(scene, camera);
+
+    document.body.appendChild(renderer.domElement);
+}
