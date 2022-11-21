797c3348cc19cb7887494c6c350e6577d63340fa
Michele Nardini <michele.nardini3g@gmail.com>
2022-11-21 16:10:57 +0100

    update

--- webXRCameraAccess/ARRawAccessButton.js
+++ webXRCameraAccess/ARRawAccessButton.js
@@ -1,37 +1,229 @@
-class ARRawAccessButton{
-    static createButton(renderer, sessionInit={}){
-        const button = document.createElement('button');
-        
-        if('xr' in navigator){
-            button.id='ARButton';
-            button.style.display='none';
+import {createPlaneMarker, createMesh} from './scene.js';
+import * as THREE from 'https://unpkg.com/three@0.146.0/build/three.module.js';
 
-            stylizeElement(button);
-            navigator.xr.isSessionSupported('immersive-ar').then(function(supported){
-                supported ? showStartAR() : showARNotSupported();
-            }).catch(showARNotAllowed);
-            return button;
-        }else{
-            const message = document.createElement('a');
+class ARRawAccessButton {
 
-            if(window.isSecureContext == false){
-                message.href=document.location.href.replace(/^http:/,'https.');
-                message.innerHTML='WEBXr NEEDS HTML';
-            }else{
-                message.href='https://immersiveweb.dev/';
-                message.innerHTML='WEBXR NOT AVAILABLE';
-            }
+	static createButton( renderer, sessionInit = {} ) {
 
-            message.style.left='calc(50% - 90px)';
-            message.style.width='180px';
-            message.style.textDecoration='none';
+		const button = document.createElement( 'button' );
+		let hitTestSource = null;
+		let hitTestSourceRequested = false;
+		let camera, scene, box, container;
+		let controller;
+		let gl, glBinding;
 
-            stylizeElement(message);
+		init();
 
-            return message;
-        }
+		function init(){
+			container = document.createElement('div');
+			document.body.appendChild(container);
 
-        function stylizeElement( element ) {
+			scene = new THREE.Scene();
+
+			camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.02, 20);
+
+
+			const ambientLight = new THREE.AmbientLight(0xffffff, 1.0);
+			scene.add(ambientLight);
+
+			box = new THREE.Mesh(new THREE.BoxGeometry(1,1,1), new THREE.MeshBasicMaterial({color: 0xff0000}));
+			box.position.z =-3;
+			scene.add(box);
+			container.appendChild(renderer.domElement);
+
+			
+			controller = renderer.xr.getController(0);
+
+			controller.addEventListener('select', function () {
+				if (planeMarker.visible) {
+					scene.add(createMesh(planeMarker.matrix));
+				}
+			});
+
+			scene.add(controller);
+		}
+
+		function showStartAR( /*device*/ ) {
+
+			if ( sessionInit.domOverlay === undefined ) {
+
+				const overlay = document.createElement( 'div' );
+				overlay.style.display = 'none';
+				document.body.appendChild( overlay );
+
+				const svg = document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' );
+				svg.setAttribute( 'width', 38 );
+				svg.setAttribute( 'height', 38 );
+				svg.style.position = 'absolute';
+				svg.style.right = '20px';
+				svg.style.top = '20px';
+				svg.addEventListener( 'click', function () {
+
+					currentSession.end();
+
+				} );
+				overlay.appendChild( svg );
+
+				const path = document.createElementNS( 'http://www.w3.org/2000/svg', 'path' );
+				path.setAttribute( 'd', 'M 12,12 L 28,28 M 28,12 12,28' );
+				path.setAttribute( 'stroke', '#fff' );
+				path.setAttribute( 'stroke-width', 2 );
+				svg.appendChild( path );
+
+				if ( sessionInit.optionalFeatures === undefined ) {
+
+					sessionInit.optionalFeatures = [];
+
+				}
+
+				sessionInit.optionalFeatures.push( 'dom-overlay' );
+				sessionInit.domOverlay = { root: overlay };
+
+			}
+
+			//
+
+			let currentSession = null;
+
+			async function onSessionStarted( session ) {
+
+				session.addEventListener( 'end', onSessionEnded );
+				
+
+				renderer.xr.setReferenceSpaceType( 'local' );
+
+				gl = renderer.getContext();
+
+				await renderer.xr.setSession( session ).then(ses => {
+					gl.makeXRCompatible().then((x)=>{
+						console.log(session);
+						glBinding = new XRWebGLBinding(session, gl);
+					}).catch(e=>{
+						console.log(e);
+					});
+				});
+
+			
+				button.textContent = 'STOP AR';
+				sessionInit.domOverlay.root.style.display = '';
+
+				currentSession = session;
+
+				currentSession.requestAnimationFrame(onXRFrame);
+
+			}
+
+			function onSessionEnded( /*event*/ ) {
+
+				currentSession.removeEventListener( 'end', onSessionEnded );
+
+				button.textContent = 'START AR';
+				sessionInit.domOverlay.root.style.display = 'none';
+
+				currentSession = null;
+
+			}
+
+			//
+
+			button.style.display = '';
+
+			button.style.cursor = 'pointer';
+			button.style.left = 'calc(50% - 50px)';
+			button.style.width = '100px';
+
+			button.textContent = 'START AR';
+
+			button.onmouseenter = function () {
+
+				button.style.opacity = '1.0';
+
+			};
+
+			button.onmouseleave = function () {
+
+				button.style.opacity = '0.5';
+
+			};
+
+			button.onclick = function () {
+
+				if ( currentSession === null ) {
+
+					navigator.xr.requestSession( 'immersive-ar', sessionInit ).then( onSessionStarted );
+
+				} else {
+
+					currentSession.end();
+
+				}
+
+			};
+
+		}
+
+		function onXRFrame(time, frame) {
+			const session = frame.session;
+			session.requestAnimationFrame(onXRFrame);
+			if (frame) {
+
+				const referenceSpace = renderer.xr.getReferenceSpace();
+				const session = renderer.xr.getSession();
+				let pose = frame.getViewerPose(referenceSpace);
+
+				if(pose){
+					for(const view of pose.views){
+						if(view.camera){
+							const cameraTexture=glBinding.getCameraImage(view.camera);
+							const texture = new THREE.TextureLoader().load(cameraTexture);
+
+							
+							const material = new THREE.MeshBasicMaterial( { map: texture } );
+
+							
+							box.material = material;
+						}
+					}
+				}
+		
+			}
+			renderer.render(scene, camera);
+		}
+
+		function disableButton() {
+
+			button.style.display = '';
+
+			button.style.cursor = 'auto';
+			button.style.left = 'calc(50% - 75px)';
+			button.style.width = '150px';
+
+			button.onmouseenter = null;
+			button.onmouseleave = null;
+
+			button.onclick = null;
+
+		}
+
+		function showARNotSupported() {
+
+			disableButton();
+
+			button.textContent = 'AR NOT SUPPORTED';
+
+		}
+
+		function showARNotAllowed( exception ) {
+
+			disableButton();
+
+			console.warn( 'Exception when trying to call xr.isSessionSupported', exception );
+
+			button.textContent = 'AR NOT ALLOWED';
+
+		}
+
+		function stylizeElement( element ) {
 
 			element.style.position = 'absolute';
 			element.style.bottom = '20px';
@@ -48,19 +240,49 @@ class ARRawAccessButton{
 
 		}
 
-        function showStartAR(){
-            console.log("Supported");
-        }
+		if ( 'xr' in navigator ) {
 
-        function showARNotSupported(){
-            console.log("not supported");
-        }
+			button.id = 'ARButton';
+			button.style.display = 'none';
 
-        function showARNotAllowed(){
+			stylizeElement( button );
 
-        }
+			navigator.xr.isSessionSupported( 'immersive-ar' ).then( function ( supported ) {
 
-    }
+				supported ? showStartAR() : showARNotSupported();
+
+			} ).catch( showARNotAllowed );
+
+			return button;
+
+		} else {
+
+			const message = document.createElement( 'a' );
+
+			if ( window.isSecureContext === false ) {
+
+				message.href = document.location.href.replace( /^http:/, 'https:' );
+				message.innerHTML = 'WEBXR NEEDS HTTPS'; // TODO Improve message
+
+			} else {
+
+				message.href = 'https://immersiveweb.dev/';
+				message.innerHTML = 'WEBXR NOT AVAILABLE';
+
+			}
+
+			message.style.left = 'calc(50% - 90px)';
+			message.style.width = '180px';
+			message.style.textDecoration = 'none';
+
+			stylizeElement( message );
+
+			return message;
+
+		}
+
+	}
+
 }
 
-export {ARRawAccessButton};
+export { ARRawAccessButton };
--- webXRCameraAccess/main.js
+++ webXRCameraAccess/main.js
@@ -1,53 +1,20 @@
 import * as THREE from 'https://unpkg.com/three@0.146.0/build/three.module.js';
 import { ARRawAccessButton } from './ARRawAccessButton.js';
-import {createPlaneMarker, createMesh} from './scene.js'
 
-let container;
-let camera, scene, renderer, cube;
 
 
 init();
-animate();
+
 
 function init() {
 
-    container = document.createElement('div');
-    document.body.appendChild(container);
-
-    scene = new THREE.Scene();
-
-    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.02, 20);
-
-
-    const ambientLight = new THREE.AmbientLight(0xffffff, 1.0);
-    scene.add(ambientLight);
-
-    renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+    const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.xr.enabled = true;
-    container.appendChild(renderer.domElement);
-
-    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
-    cube = new THREE.Mesh( geometry, material );
-    scene.add(cube);
 
 
-    document.body.appendChild(ARRawAccessButton.createButton(renderer, { requiredFeatures: ['camera-access'] }));
+    document.body.appendChild(ARRawAccessButton.createButton(renderer, { requiredFeatures: ['hit-test','camera-access'] }));
 
 }
 
-
-function animate() {
-    renderer.setAnimationLoop(render);
-
-}
-
-function render(timestamp, frame) {
-
-    
-
-    renderer.render(scene, camera);
-
-}
